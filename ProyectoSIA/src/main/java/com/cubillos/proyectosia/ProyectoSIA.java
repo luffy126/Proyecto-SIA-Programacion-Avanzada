b6361991099b4d51f5d4bd6bb3a792610d157040
Simón Ledezma <simon.ledezma2005@gmail.com>
2025-08-23 01:41:31 -0400

    hola

--- ProyectoSIA/src/main/java/com/cubillos/proyectosia/ProyectoSIA.java
+++ ProyectoSIA/src/main/java/com/cubillos/proyectosia/ProyectoSIA.java
@@ -11,6 +11,6 @@ package com.cubillos.proyectosia;
 public class ProyectoSIA {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("el pepe ete sech");
     }
 }
