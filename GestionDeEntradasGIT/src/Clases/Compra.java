4d4b981673aa5501f4785fafaeb0457696c5610e
Panchiky <fraan16poncee@gmai.com>
2025-08-25 18:42:27 -0400

    Crear clase comprar: - Agregar sus atributos. - Implementar constructor con parametros. - Agregar metodos getters y setters.

--- GestionDeEntradasGIT/src/Clases/Compra.java
+++ GestionDeEntradasGIT/src/Clases/Compra.java
@@ -1,25 +1,42 @@
 package Clases;
+import java.time.LocalDate;
 
 /**
  *
- * @author Tenerex
+ * @author Panchiky
  */
+//Clase y sus atributos
 public class Compra {
-    int ordenDeCompra;
-    int montoTotal;
-    String fechaCompra;
-    String estadoCompra;
+    private int ordenDeCompra;
+    private String rut;
+    private String formaDePago;
+    private LocalDate fechaDeCompra;
+    private String estadoDeCompra;
+    private int montoTotal;
     
-    
-    public Compra () {
-    
-        
+    //constructor con parámetros 
+    public Compra (int ordenDeCompra, String rut, String formaDePago, LocalDate fechaDeCompra, String estadoDeCompra, int montoTotal) {
+        this.ordenDeCompra =  ordenDeCompra;
+        this.rut = rut;
+        this.formaDePago = formaDePago;
+        this.fechaDeCompra = fechaDeCompra;
+        this.estadoDeCompra = estadoDeCompra;
+        this.montoTotal = montoTotal;
     }
     
-    
-    
+    //GETTERS
+    public int getOrdenDeCompra(){return ordenDeCompra;}
+    public String getRut(){return rut;}
+    public String getFormaDePago(){return formaDePago;}
+    public LocalDate getFechaDeCompra(){return fechaDeCompra;}
+    public String getEstadoDeCompra(){return estadoDeCompra;}
+    public int getMontoTotal(){return montoTotal;}
     
     // SETTERS
-    
-    
+    public void setOrdenDeCompra(int ordenDeCompra){this.ordenDeCompra = ordenDeCompra;}
+    public void setRut(String rut){this.rut = rut;}
+    public void setFormaDePago(String formaDePago){this.formaDePago = formaDePago;}
+    public void setFechaDeCompra(LocalDate fechaDeCompra){this.fechaDeCompra = fechaDeCompra;}
+    public void setEstadoDeCompra(String estadoDeCompra){this.estadoDeCompra = estadoDeCompra;}
+    public void setMontoTotal(int montoTotal){this.montoTotal = montoTotal;}
 }
